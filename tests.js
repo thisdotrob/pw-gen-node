cb9d22d958e0d095be05b4b9dc8132556dc01d8f
thisdotrob <robstevenson000@gmail.com>
2017-03-21 18:27:31 +0000

    Test that generatePassword throws if all parameters are false

--- tests.js
+++ tests.js
@@ -4,13 +4,18 @@ const assert = require('assert');
 
 const generatePasssword = require('./generate-password');
 
-describe('generatePasssword', () => {
-  it('should return a string', () => {
-    assert.equal(typeof generatePasssword(), 'string');
+describe('generatePasssword', () => {it('should return a string', () => {
+    assert.equal(typeof generatePasssword(10, true, true, true, true), 'string');
   });
 
   it('should return a password with the correct length', () => {
     const expectedLength = 20;
-    assert.equal(generatePasssword(expectedLength).length, expectedLength);
+    assert.equal(generatePasssword(20, true, true, true, true).length, expectedLength);
   });
+
+  describe('when the rules given can not be valid as all options are set to false', () => {
+    it('should throw an error', () => {
+      assert.throws(() => generatePasssword(10, false, false, false, false));
+    });
+  })
 });
