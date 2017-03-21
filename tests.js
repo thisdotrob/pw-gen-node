77631d0da2d411e42c69f7bb227980b8a64867c2
thisdotrob <robstevenson000@gmail.com>
2017-03-21 19:37:25 +0000

    Test that generatePassword only returns capitals if parameter is true

--- tests.js
+++ tests.js
@@ -2,20 +2,27 @@
 
 const assert = require('assert');
 
-const generatePasssword = require('./generate-password');
+const generatePassword = require('./generate-password');
 
-describe('generatePasssword', () => {it('should return a string', () => {
-    assert.equal(typeof generatePasssword(10, true, true, true, true), 'string');
+describe('generatePasssword', () => {
+  it('should return a string', () => {
+    assert.equal(typeof generatePassword(10, true, true, true, true), 'string');
   });
 
   it('should return a password with the correct length', () => {
-    const expectedLength = 20;
-    assert.equal(generatePasssword(20, true, true, true, true).length, expectedLength);
+    assert.equal(generatePassword(20, true, true, true, true).length, 20);
   });
 
   describe('when the rules given can not be valid as all options are set to false', () => {
     it('should throw an error', () => {
-      assert.throws(() => generatePasssword(10, false, false, false, false));
+      assert.throws(() => generatePassword(10, false, false, false, false));
     });
-  })
+  });
+
+  describe('when only \'uppercase\' is set to true', () => {
+    it('should return a password with only uppercase characters', () => {
+      const generated = generatePassword(10, true, false, false, false);
+      assert(/^[A-Z]/.test(generated));
+    });
+  });
 });
