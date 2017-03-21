d4ef7e8f59376f591cab0a5fd048baef671d9842
thisdotrob <robstevenson000@gmail.com>
2017-03-21 23:00:00 +0000

    Return mix of lowercase and uppercase a

--- generate-password.js
+++ generate-password.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function generatePasssword(length, uppercase, lowercase, number, special) {
-  if (!uppercase) throw new Error();
-  return "A".repeat(length);
+  const characterSet = [];
+
+  if (uppercase) characterSet.push('A');
+  if (lowercase) characterSet.push('a');
+
+  if (!characterSet.length) throw new Error();
+
+  let characterSetIndex = 0;
+
+  const result = [];
+
+  for (let i = 0; i < length; i++) {
+    result.push(characterSet[characterSetIndex]);
+    characterSetIndex += 1;
+    if (characterSetIndex === characterSet.length) characterSetIndex = 0;
+  }
+
+  return result.join("");
 }
 
 module.exports = generatePasssword;
