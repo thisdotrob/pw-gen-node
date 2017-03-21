ebf94355ece25972fb97f15e53e9b2d00235032f
thisdotrob <robstevenson000@gmail.com>
2017-03-21 19:37:55 +0000

    Make generatePassword use capital 'A' for all chars

--- generate-password.js
+++ generate-password.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function generatePasssword(length, uppercase, lowercase, number, special) {
-  if (!uppercase) throw new Error(); 
-  return " ".repeat(length);
+  if (!uppercase) throw new Error();
+  return "A".repeat(length);
 }
 
 module.exports = generatePasssword;
